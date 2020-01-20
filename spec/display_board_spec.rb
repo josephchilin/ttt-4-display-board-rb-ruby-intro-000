70dbb3c8adc986e65b92f0b95213706cfb44fedf
amazing-callback-3381 <joseph@josephchilin.com>
2020-01-20 23:04:15 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -179,6 +179,15 @@ describe "#display_board in 'lib/display_board.rb" do
       
       board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
       
+      output = capture_puts{ display_board(board) } if defined?(display_board)
+      rows = output.split("\n")
+      
+       expect(rows[0]).to eq(" O | O | O ")
+       expect(rows[1]).to eq("-----------")
+       expect(rows[2]).to eq(" O | O | O ")
+       expect(rows[3]).to eq("-----------")
+       expect(rows[4]).to eq(" O | O | O ")
+      
       # Can you copy the syntax of the tests above to write a test for a board
       # entirely filled with Os?
 
